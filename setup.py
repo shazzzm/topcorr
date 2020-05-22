7179ac50b10293c36560d1d3bf557fd50d6aa708
Tristan Millington <tristan.millington@gmail.com>
2020-05-22 17:30:12 +0100

    Added a setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name='topcorr',  
+    version='0.1',
+    author="Tristan Millington",
+    author_email="tristan.millington@gmail.com",
+    description="A package for consutructing filtered correlation networks",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/shazzzm/topcorr",
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+
+ )
